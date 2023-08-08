4771c3fd499972f1f983114fe0f8687706dd8a69
Connor Smith <connor.smith.256@gmail.com>
2023-08-08 15:54:23 -0600

    download big model

--- bindle/scripts/bindle_create.sh
+++ bindle/scripts/bindle_create.sh
@@ -3,6 +3,8 @@
 BINDLE_SCRIPTS=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 #source "${BINDLE_SCRIPTS}/../../deploy/env"
 
+curl -L -o $BINDLE_SCRIPTS/../models/resnet152-v2-7.onnx "https://github.com/onnx/models/blob/main/vision/classification/resnet/model/resnet152-v2-7.onnx?raw=true"
+
 for model in $(cat $BINDLE_SCRIPTS/../models/models.txt); do
   $BINDLE_SCRIPTS/push_bindle.sh $BINDLE_SCRIPTS/../models/$model-signed.toml $BINDLE_SCRIPTS/../models/$model.csv
 done
